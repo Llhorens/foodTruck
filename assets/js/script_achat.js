e425d1db44e045c1a6e1a1dc200f24dd756a822f
LudovicL <ludoviclhorens@gmail.com>
2021-10-12 14:39:56 +0200

    update

---
+++ assets/js/script_achat.js
@@ -0,0 +1,33 @@
+export * from './script_achat'
+
+const menuBtns = document.querySelectorAll('.menu-btn');
+const foodItems = document.querySelectorAll('.food-item');
+
+let activeBtn = "hamburger";
+
+showFoodMenu(activeBtn);
+
+menuBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        resetActiveBtn();
+        showFoodMenu(btn.id);
+        btn.classList.add('active-btn');
+    });
+});
+
+function resetActiveBtn(){
+    menuBtns.forEach((btn) => {
+        btn.classList.remove('active-btn');
+    });
+}
+
+function showFoodMenu(newMenuBtn){
+    activeBtn = newMenuBtn;
+    foodItems.forEach((item) => {
+        if(item.classList.contains(activeBtn)){
+            item.style.display = "grid";
+        } else {
+            item.style.display = "none";
+        }
+    });
+}
