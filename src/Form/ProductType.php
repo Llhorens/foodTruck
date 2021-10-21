e3e91d32f3b7c4c65ea66f4c65e5d0cd98376168
LudovicL <ludoviclhorens@gmail.com>
2021-10-21 14:41:02 +0200

    before merge2

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,8 +17,10 @@ class ProductType extends AbstractType
             ->add('name')
             ->add('price')
             ->add('category')
-            ->add('picture')
-            ->add('ingredient');
+            ->add('ingredient')
+            ->add('imageFile', FileType::class, [
+                'required' => false
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
