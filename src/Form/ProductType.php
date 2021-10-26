840a5702689396112cd4f72e25daf6acdeaa0862
LudovicL <ludoviclhorens@gmail.com>
2021-10-26 15:43:23 +0200

    category many

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\Product;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
@@ -16,9 +18,14 @@ class ProductType extends AbstractType
         $builder
             ->add('name')
             ->add('price')
-            ->add('category')
             ->add('ingredient')
             ->add('imageFile', FileType::class, [
+                'required' => false
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'multiple' => true,
                 'required' => false
             ]);
     }
